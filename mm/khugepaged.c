d302954aa3d37128f33a563d9f4e35e304441e0b
ncatt <anntuongg@gmail.com>
2026-02-28 21:08:46 +0700

    khugepaged.c: fuck.

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1317,7 +1317,7 @@ static void retract_page_tables(struct address_space *mapping, pgoff_t pgoff)
 				_pmd = pmdp_collapse_flush(vma, addr, pmd);
 				spin_unlock(ptl);
 				mm_dec_nr_ptes(mm);
-				flush_tlb_mm_range(mm);
+				flush_tlb_mm(mm);
 				tlb_remove_table_sync_one();
 				pte_free(mm, pmd_pgtable(_pmd));
 				mmu_notifier_invalidate_range_end(mm, addr, end);
